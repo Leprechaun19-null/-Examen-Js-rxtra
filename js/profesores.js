7fe74f8c6977c1da95a531a76a313f59bf6ac920
Manuel Pop <manpop2@alu.edu.gva.es>
2026-06-26 09:53:44 +0200

    commit

--- js/profesores.js
+++ js/profesores.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded",  main())
 function main(){
-const 
+const elementonombre=document.getElementById("p-nombre");
+const elementodni=document.getElementById("p-dni");
+const elementotelefono=document.getElementById("p-telefono");
+const elementocourso=document.getElementById("p-curso");
+
 }
